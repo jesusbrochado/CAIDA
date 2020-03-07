936ef8c85abddde5694d029ba8c5c4ddf1e23993
Tony Yu <tonyyucen@gmail.com>
2020-03-07 14:35:34 -0500

    using findall for proposal

--- bin/caida.py
+++ bin/caida.py
@@ -5,32 +5,19 @@ from code import lambdas, functions
 import re
 
 ## DECLARATIONS
-userLog = lambdas.readDebugs('../pub/debugs/userlog.txt') # is constant for now!!!
+userLog = lambdas.readDebugs('../pub/debugs/userlog2.txt') # is constant for now!!!
 filters = lambdas.csvToDict('logs_init.csv')
 
 # Here we extract key data from the string get userLog that user enter to the system.
 iniciator = True if re.search(r'Received PFKEY Acquire SA for SPI 0x0, error FALSE',userLog) else  False
 peer = re.search('attempting to find tunnel group for IP:(.+?)\n', userLog).group(1)
-proposalType = re.search('my_auth_method = (.+?)\n', userLog).group(1) #Proposal type: 1 PKI, 2 PSK
+proposalType = "PKI" if re.search('my_auth_method = (.+?)\n', userLog).group(1) == 1 else "PSK" #Proposal type: 1 PKI, 2 PSK
 tunnelType = re.search('tunn grp type set to: (.+?)\n', userLog).group(1)
 
 #Special case this must be a collection NEED REFIX IT
 proposal_phase_1 = re.search('Proposal: (.+?), Protocol', userLog).group(1)
 protocol_phase_1 = re.search('Protocol id: (.+?), SPI', userLog).group(1)
-# PLEASE DONT FORGET THESE TOO:
-#type: 1, reserved: 0x0, id: AES-CBC
-#type: 2, reserved: 0x0, id: SHA256
-#type: 3, reserved: 0x0, id: SHA256
-#type: 4, reserved: 0x0, id: DH_GROUP_2048_MODP/Group 14
-#(2): IKEv2 IKE_SA_INIT Exchange RESPONSEIKEv2-PROTO-3: (2): Next payload: SA, version: 2.0 (2): Exchange type: IKE_SA_INIT, flags: RESPONDER MSG-RESPONSE (2): Message id: 0, length: 574(2):
-
-# I AM NOT CLEAR YET, may is the up forward
-# type: 1, reserved: 0x0, id: AES-CBC
-# type: 2, reserved: 0x0, id: SHA256
-# type: 3, reserved: 0x0, id: SHA256
-# type: 4, reserved: 0x0, id: DH_GROUP_2048_MODP/Group 14
-
-phase_1 = True if re.search(r'(I) MsgID = 00000000 CurState: INIT_DONE Event: EV_CHK4_ROLE',userLog) else  False
+phase_1 = True if re.search(r'\(I\) MsgID = 00000000 CurState: INIT_DONE Event: EV_CHK4_ROLE',userLog) else  False
 
 # Phase 2 extractiong
 noNATfound = True if re.search(r'No NAT found',userLog) else  False
@@ -42,15 +29,15 @@ localAuthentication = re.search("My authentication method is '(.+?)'", userLog).
 
 # proposal_phase_2 // wait
 # protocol_phase_2 // wait
- 
+
 proposal_number_phase_2 = re.search('Num of TSs: (.+?), reserved 0x0, reserved 0x0', userLog).group(1)
-peerAuthenticationType = re.search('peer auth method set to: (.+?)\n', userLog).group(1) # 1 PKI, 2 PSK
+peerAuthenticationType = "PKI" if re.search('peer auth method set to: (.+?)\n', userLog).group(1) == 1 else "PSK"# 1 PKI, 2 PSK
 peerAuthenticationComplete = True if re.search(r'Completed authentication for connection',userLog) else  False
 idleTimeout = re.search('idle timeout set to: (.+?)\n', userLog).group(1)
 sessionTimeout = re.search('session timeout set to: (.+?)\n', userLog).group(1)
 nameGroupPolicy = re.search('group policy set to (.+?)\n', userLog).group(1)
 DPDtimer = re.search('Initializing DPD, configured for (.+?) seconds', userLog).group(1)
-cryptoMapSecuence = re.search('PROXY MATCH on crypto map CRY_MAP seq (.+?)\n', userLog).group(1)
+cryptoMapSecuence = re.search('PROXY MATCH on crypto map (.+?) seq (.+?)\n', userLog).group(1)
 I_SPI = re.search('SM Trace-> SA: I_SPI=(.+?) R_SPI=', userLog).group(1)
 ## Add ignore some value for regex
 R_SPI = re.search('SM Trace-> SA: I_SPI=' + I_SPI + ' R_SPI=(.+?) \(I\) MsgID = 00000001 CurState: READY Event:', userLog).group(1)
@@ -77,9 +64,12 @@ print("cryptoMapSecuence: ", cryptoMapSecuence)
 print("I_SPI: ", I_SPI)
 print("R_SPI: ", R_SPI)
 print("tunelUp: ", tunelUp)
+print("phase_1: ", phase_1)
 
 
+p1_proposal = re.findall('Proposal: (.+?). Protocol id: (.+?), SPI size: 0, #trans: 4', userLog)
 
+print(p1_proposal)
 
 #dici = [['\n'+'Your initial configuration is :'+'\n']]
 #functions.conf_ini(initiator,filters,dici)
