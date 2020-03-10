f52c1a28aeb0887544742cb2aa6fa13f64931b39
MrCelis <miguelangelcelis97@gmail.com>
2020-03-10 18:30:12 -0500

    Update 10 Marzo - Varios problemas que corregir see Trello

--- bin/caida.py
+++ bin/caida.py
@@ -5,64 +5,84 @@ from code import lambdas, functions
 import re
 
 ## DECLARATIONS
-filePath = '../pub/debugs/userlog2.txt'
+filePath = '../pub/debugs/init_NAT_T_Working.txt'
 userLog = lambdas.readDebugs(filePath) # is constant for now!!!
 filters = lambdas.csvToDict('logs_init.csv')
 
 
 # Here we extract key data from the string get userLog that user enter to the system.
-iniciator = True if re.search(r'Received PFKEY Acquire SA for SPI 0x0, error FALSE',userLog) else  False
-peer = re.search('attempting to find tunnel group for IP:(.+?)\n', userLog).group(1)
-proposalType = "PKI" if re.search('my_auth_method = (.+?)\n', userLog).group(1) == 1 else "PSK" #Proposal type: 1 PKI, 2 PSK
-tunnelType = re.search('tunn grp type set to: (.+?)\n', userLog).group(1)
+iniciator = True if re.search(r'EV_INIT_SA',userLog) else  False
+
+#PENDING!
+#peer = re.search('attempting to find tunnel group for IP:(.+?)\n', userLog).group(1)    Si esto retorna vacio entonces lo de abajo
+peer = re.search('Sending Packet \[To (.+?)', userLog).group(1)
+
+#PENDING!
+#proposalType = "PKI" if re.search('my_auth_method = (.+?)\n', userLog).group(1) == 1 else "PSK" #Proposal type: 1 PKI, 2 PSK y si no sale entonces "My authentication method is"
+proposalType = "PKI" if re.search('My authentication method is (.+?)\n', userLog).group(1) == 1 else "PSK"
+
+#tunnelType = re.search('tunn grp type set to: (.+?)\n', userLog).group(1) #"site to site by default, si encuentre entonces el string que encuentre"
 
 #Special case this must be a collection NEED REFIX IT
 proposal_phase_1 = re.search('Proposal: (.+?), Protocol', userLog).group(1)
 protocol_phase_1 = re.search('Protocol id: (.+?), SPI', userLog).group(1)
 phase_1 = True if re.search(r'\(I\) MsgID = 00000000 CurState: INIT_DONE Event: EV_CHK4_ROLE',userLog) else  False
 
-# Phase 2 extractiong
+# NAT Detection
 noNATfound = True if re.search(r'No NAT found',userLog) else  False
+us_NAT_T = True if re.search(r'NAT INSIDE found',userLog) else  False
+remote_NAT_T = True if re.search(r'NAT OUTSIDE found',userLog) else  False
+
+
+
+
 
 # localKeyLength #Question what establish if is remote or local?
 # remoteKeyLength #Question what establish if is remote or local?
 
 localAuthentication = re.search("My authentication method is '(.+?)'", userLog).group(1)
 
-# proposal_phase_2 // wait
+# proposal_phase_2 // wait 
 # protocol_phase_2 // wait
 
 proposal_number_phase_2 = re.search('Num of TSs: (.+?), reserved 0x0, reserved 0x0', userLog).group(1)
-peerAuthenticationType = "PKI" if re.search('peer auth method set to: (.+?)\n', userLog).group(1) == 1 else "PSK"# 1 PKI, 2 PSK
+
+#PENDING!
+#peerAuthenticationType = "PKI" if re.search('peer auth method set to: (.+?)\n', userLog).group(1) == 1 else "PSK"# 1 PKI, 2 PSK  O si no hace match entonces "Peer's authentication method is" toca poner bien la comilla
+#peerAuthenticationType = "PKI" if re.search('Peer\'s authentication method is \n', userLog).group(1) == 1 else "PSK"
 peerAuthenticationComplete = True if re.search(r'Completed authentication for connection',userLog) else  False
 idleTimeout = re.search('idle timeout set to: (.+?)\n', userLog).group(1)
 sessionTimeout = re.search('session timeout set to: (.+?)\n', userLog).group(1)
 nameGroupPolicy = re.search('group policy set to (.+?)\n', userLog).group(1)
 DPDtimer = re.search('Initializing DPD, configured for (.+?) seconds', userLog).group(1)
-cryptoMapSecuence = re.search('PROXY MATCH on crypto map (.+?) seq (.+?)\n', userLog).group(1)
+#PENDING!
+#cryptoMapSecuence = re.search('PROXY MATCH on crypto map (.+?) seq (.+?)\n', userLog).group(1)
 I_SPI = re.search('SM Trace-> SA: I_SPI=(.+?) R_SPI=', userLog).group(1)
 ## Add ignore some value for regex
 R_SPI = re.search('SM Trace-> SA: I_SPI=' + I_SPI + ' R_SPI=(.+?) \(I\) MsgID = 00000001 CurState: READY Event:', userLog).group(1)
 tunelUp = True if re.search(r'CurState: READY Event: EV_I_OK',userLog) else  False
 
 
+
 print("iniciator: ", iniciator)
 print("peer: ", peer)
 print("proposalType: ", proposalType)
-print("tunnelType: ", tunnelType)
+#print("tunnelType: ", tunnelType)
 print("proposal_phase_1", proposal_phase_1)
 print("protocol_phase_1", protocol_phase_1)
 print("Phase 1 is enable: ", phase_1)
-print("No NAT found: ", noNATfound)
+print("No NAT: ", noNATfound)
+print("NAT T Local: ", us_NAT_T)
+print("NAT T Remote: ", remote_NAT_T)
 print("localAuthentication: ", localAuthentication)
 print("proposal_number_phase_2: ", proposal_number_phase_2)
-print("authenticationPeerType: ", peerAuthenticationType)
+#print("authenticationPeerType: ", peerAuthenticationType)
 print("peerAuthenticationComplete: ", peerAuthenticationComplete)
 print("idleTimeout: ", idleTimeout)
 print("sessionTimeout: ", sessionTimeout)
 print("nameGroupPolicy: ", nameGroupPolicy)
 print("DPDtimer: ", DPDtimer)
-print("cryptoMapSecuence: ", cryptoMapSecuence)
+#print("cryptoMapSecuence: ", cryptoMapSecuence)
 print("I_SPI: ", I_SPI)
 print("R_SPI: ", R_SPI)
 print("tunelUp: ", tunelUp)
@@ -94,6 +114,9 @@ p1_resp = filterProposal('Exchange type: IKE_SA_INIT, flags: RESPONDER MSG-RESPO
 p2_prop = filterProposal('Protocol id: ESP, SPI size:', '\):   Next payload: TSr')
 sa_traffic_init_local = filterProposal('\):   Next payload: TSr', 'TSr(.+?):   Next payload: NOTIFY, reserve')
 sa_traffic_init_remote = filterProposal('TSr(.+?):   Next payload: NOTIFY, reserve', 'NOTIFY\(INITIAL_CONTACT\)')
+
+#PENDINTE! SI LO DE ABAJO VACIO, ENTONCES USAR LA ULTIMA POSICION DE LAS LISTAS DE sa_traffic_init_local Y sa_traffic_init_remote
+
 sa_traffic_agreed_local= filterProposal('TSi  Next payload: TSr', ' TSr  Next payload: NOTIFY, res')
 sa_traffic_agreed_remote= filterProposal('TSr  Next payload: NOTIFY', 'CurState: I_WAIT_AUTH Event: EV_RECV_AUTH')
 
